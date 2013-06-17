dfc0ca08c5f3fe88806eb5d805b4c398aef08cef
dodo <your.dodo7071@azet.com>
2013-06-17 18:15:21 +0200

    v0.8

--- db/migrate/20130416213028_add_keys.rb
+++
@@ -1,9 +0,0 @@
-class AddKeys < ActiveRecord::Migration
-  def change
-    add_foreign_key "activities", "activity_types", :name => "activities_activity_type_id_fk"
-    add_foreign_key "activities", "locations", :name => "activities_location_id_fk"
-    add_foreign_key "activities", "users", :name => "activities_user_id_fk"
-    add_foreign_key "locations", "regions", :name => "locations_region_id_fk"
-    add_foreign_key "user_activities", "users", :name => "user_activities_user_id_fk"
-  end
-end
